c00b2ae6f0c6a3bf8497f29ad4d5e0e8f8d7c846
hdsef <31828672+hdsef@users.noreply.github.com>
2018-03-12 17:28:47 +0300

    Add files via upload

--- P2P.py
+++ P2P.py
@@ -97,8 +97,12 @@ def Sending(Actual_chat):
 
 
 print("Please choose login")
-IPaddr = (([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")] or [[(s.connect(
-    ("8.8.8.8", 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) + ["no IP found"])[0]
+#IPaddr = (([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")] or [[(s.connect(
+#    ("8.8.8.8", 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) + ["no IP found"])[0]
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(("gmail.com",80))
+IPaddr=s.getsockname()[0]
+s.close()
 message = input()
 MESSAGE = IPaddr+"@#"+message
 users.append(MESSAGE)
